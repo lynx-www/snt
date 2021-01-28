dc695c20559c121b1909f3f7d18c77c77f13ed6a
lynx-www <Ira197708@yandex.ru>
2021-01-28 17:38:29 +0300

    initial commit

--- view/dolgs.php
+++ view/dolgs.php
@@ -1,8 +1,10 @@
 <?php
 include('conf.php');
 $dolg = new DataBase();
-$sql = "SELECT * FROM street, sector WHERE street.id = sector.street AND sector.status = 1 AND sector.id LIKE '001'";
+$sql = "SELECT * FROM street, sector WHERE street.id = sector.street AND sector.status = 1";
 $dolgs = $dolg->select_sql($sql);
+$itog_dolg = 0;
+$itog_peny = 0;
 echo '<table class="table table-sm table-striped table-reflow">';
 
 
@@ -11,11 +13,11 @@ foreach($dolgs as $d){
     echo '<tr><th style="background: #ccc;">Участок '.$d['id'].'</th><th colspan="5" style="background: #ccc;">'.$d['name'].'</th></tr>';
     //echo '<tr><th>Наименование</th><th>Сумма к оплате</th></tr>';
    echo '</thead>';
-    $opl = $dolg->one('plot', 'oplats1', $d['id']);
+    $opl = $dolg->one('plot', 'oplats', $d['id']);
     foreach($opl as $o){
         //echo '<tbody>';
         echo '<tr><th>'.$o['name'].'</th><th>Сумма к оплате '.$o['pay'].'</th><th>Оплатить до '.$o['date_opl'].'</th>
-        <th>Просрочено дней</th><th>Реффинансирование</th><th>Пени</th></tr>';
+        <th>Просрочено дней</th><th>Пени</th></tr>';
       //  $pay = "SELECT * FROM `payment` WHERE plot LIKE '".$d['id']."' AND name = '".$o['name']."'";
       //  echo $pay;
       $pay = "SELECT oplats.plot as o_plot, oplats.name as o_name, 
@@ -26,9 +28,18 @@ foreach($dolgs as $d){
       foreach($pays as $p){
          
           $my_diff = $dolg->col_days($o['date_opl'], $p['date']);
-       
+
+          if($my_diff > 0){
+
+          $peny = $dolg->itog_peny($d['id'], $o['name']);
+
+          }
+          else {$peny = 0; }
+
+
+       //  $itog_peny = $itog_peny + $peny;
         echo '<tr><td color="red">Оплата </td><td>'.$p['pay'].'</td><td>Дата оплаты: '.$p['date'].'</td>
-        <td>'.round($my_diff, 0).'</td><td></td></tr>';
+        <td>'.round($my_diff, 0).'</td><td>'.round($peny, 2).'</td></tr>';
       }  
       $sqls = "SELECT sum(pay) as pays FROM payment WHERE plot = '".$d['id']."' AND name = '".$o['name']."' GROUP BY name";
       $itog = $dolg->one_param($sqls);
@@ -36,10 +47,13 @@ foreach($dolgs as $d){
       
       echo '<tr><td color="red">Итого оплачено </td><td>'.$itog.'</td><td></td></tr>';
       echo '<tr><td color="red">Долг </td><td>'.$ds.'</td><td></td></tr>';
+      $itog_dolg = $ds + $itog_dolg;
     }
     
 }
-
+$sum_peny = $dolg->sum_peny($d['id']);
+echo '<tr><th>Общая сумма долга на '.date('d-m-Y').' </th><th>'.$itog_dolg.'</th>
+<th>Сумма пеней </th><th>'.round($sum_peny, 2).'</th></tr>';
 echo '</table>';
 
 ?>
